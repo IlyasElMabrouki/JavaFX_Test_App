751ea995689546ec1652ab826d1276eefd951cdd
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-11-26 21:20:19 +0100

    Created custom scene

--- test_app/src/main/java/com/example/demo2/HelloApplication.java
+++ test_app/src/main/java/com/example/demo2/HelloApplication.java
@@ -1,8 +1,10 @@
 package com.example.demo2;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +12,15 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Group group = new Group();
+        Scene scene = new Scene(group, 600, 600, Color.BLUEVIOLET);
+
+        Text text = new Text();
+        text.setText("ILYAS EL MABROUKI");
+        text.setX(300);
+        text.setY(300);
+        group.getChildren().add(text);
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
