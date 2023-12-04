f153a352f9798da5be1e2f7dd27d5c03338c1eaa
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-12-04 21:43:53 +0100

    Event handling

--- test_app/src/main/java/com/example/demo2/HelloController.java
+++ test_app/src/main/java/com/example/demo2/HelloController.java
@@ -2,13 +2,30 @@ package com.example.demo2;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.shape.Circle;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Circle mycircle;
+    private double x,y;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome Ilyas El Mabrouki!");
+    public void up(){
+        //System.out.println("UP");
+        mycircle.setCenterY(y-=10);
+    }
+
+    public void left(){
+        //System.out.println("LEFT");
+        mycircle.setCenterX(x-=10);
+    }
+
+    public void right(){
+        //System.out.println("RIGHT");
+        mycircle.setCenterX(x+=10);
+    }
+
+    public void down(){
+        //System.out.println("DOWN");
+        mycircle.setCenterY(y+=10);
     }
 }
